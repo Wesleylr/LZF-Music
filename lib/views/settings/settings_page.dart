2fb95d0f5c48db60b7d65af5003fd76e4993130a
GerryDush <gerrydushkinktw19@gmail.com>
2026-03-18 23:55:16 +0800

    feat: replace ios icon

--- lib/views/settings/settings_page.dart
+++ lib/views/settings/settings_page.dart
@@ -664,7 +664,7 @@ class SettingsPageState extends State<SettingsPage> with ShowAwarePage {
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('版本: 0.4.2'),
+            Text('版本: 0.4.3'),
             SizedBox(height: 8),
             Text('基于 Flutter 开发'),
             Text('开源软件，采用 Apache 2.0 许可证'),
